2dedb82ea6d6702f2d40614248be82bd3545f8b2
Matt Claypotch <thepotch@gmail.com>
2015-03-11 11:13:59 -0700

    server side rendering woot woot

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var routes = require('./lib/routes');
+var template = require('fs').readFileSync('static/index.html').toString();
+var React = require('react');
+var Router = require('react-router');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/static'));
@@ -9,6 +13,15 @@ var server = http.createServer(app);
 
 app.get('/', function(req, res) {
   res.redirect('/index.html');
+});
+
+app.use(function (req, res) {
+  // pass in `req.url` and the router will immediately match
+  Router.run(routes, req.url, function (Handler) {
+    var content = React.renderToString(React.createElement(Handler, null));
+    var body = template.replace('<REPLACEME/>', content);
+    res.send(body);
+  });
 });
 
 server.listen(app.get('port'), function() {
