012d3802a9613e4c28fe6805f2875b381b6a1a09
lonnen <chris.lonnen@gmail.com>
2015-03-15 00:38:56 -0700

    stub out an api

--- index.js
+++ index.js
@@ -11,6 +11,75 @@ app.use(express.static(__dirname + '/static'));
 
 var server = http.createServer(app);
 
+// API
+
+/* GET /events
+ *
+ * retrieve a list of event resources
+ */
+app.get('/events', function(req, res) {
+
+});
+
+/* GET /events/{id}
+ *
+ * retrieve a single event resources
+ */
+app.get('/events/:id', function(req, res) {
+
+});
+
+/* GET /users/
+ *
+ * retrieve a list of users
+ */
+app.get('/users/', function(req, res) {
+
+});
+
+/* GET /users/{id}
+ *
+ * retrieve a single user resources
+ */
+app.get('/users/:id', function(req, res) {
+
+});
+
+/* GET /tickets/
+ *
+ * retrieve a list of tickets
+ */
+app.get('/tickets/', function(req, res) {
+
+});
+
+/* GET /tickets/{id}
+ *
+ * retrieve a single ticket resources
+ */
+app.get('/tickets/:id', function(req, res) {
+
+});
+
+/* GET /locations/
+ *
+ * retrieve a list of locations
+ */
+app.get('/tickets/:id', function(req, res) {
+
+});
+
+/* GET /locations/{id}
+ *
+ * retrieve a single location resources
+ */
+app.get('/tickets/:id', function(req, res) {
+
+});
+
+
+// views and front end
+
 app.get('/', function(req, res) {
   res.redirect('/index.html');
 });
